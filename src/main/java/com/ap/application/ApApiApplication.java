90df611b7011dd02ea2aecb39fe8a16fb068aefc
GabrielDeNobrega <sr.gabrielnobrega@hotmail.com>
2023-05-07 22:25:43 -0300

    [AP-000] Create the initial commit with a fresh project.

---
+++ src/main/java/com/ap/application/ApApiApplication.java
@@ -0,0 +1,13 @@
+package com.ap.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ap/application/ApApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
